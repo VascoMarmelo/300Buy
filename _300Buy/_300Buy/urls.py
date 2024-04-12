264eb8cf99d79d93af52c3c401b2460c433f428b
Vasco Marmelo <vascomartinsmarmelo@gmail.com>
2024-04-12 21:47:51 +0100

    Repaired web page

--- _300Buy/_300Buy/urls.py
+++ _300Buy/_300Buy/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from app.views import index
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
     path('api/', include('api.urls')),
