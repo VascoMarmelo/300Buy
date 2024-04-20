91720a477d06e6c96a1038869912bc76a650b0ee
Vasco Marmelo <vascomartinsmarmelo@gmail.com>
2024-04-20 15:29:33 +0100

    React Base 1

--- _300Buy/app/static/polls/main_page.js
+++ _300Buy/app/static/polls/main_page.js
@@ -1,16 +1,29 @@
 function App() {
+    let productsList = [];
 
-    fetch("api/products/").then(response => console.log(JSON.stringify(response.body)));
-    console.log("OLA");
-  
+    fetch("api/products").then(response => response.json())
+    .then(data => {
+        console.log(data);
+        
+        for (let p in data){
+          productsList.push(p);
+        }
+      })
+    .then(data => {return (
+      <div>
+        {productsList[0].title}
+      </div>
+    )});
+
     return (
       <div>
-        Ola
+        "OLA"
       </div>
     );
+  
+    
 }
 
 
 const domContainer = document.querySelector('#teste');
 ReactDOM.render(<App />, domContainer);
-console.log("Hello World");
