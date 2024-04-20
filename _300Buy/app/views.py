9cc887d8cbd37c0948e4e074be64c26bbc2f5d85
SpicyTauros <andr2428@gmail.com>
2024-04-20 13:33:02 +0100

    Added login and logout views

--- _300Buy/app/views.py
+++ _300Buy/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
-# from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout
 from .models import User
 
 # Create your views here.
@@ -8,5 +8,19 @@ from .models import User
 def index(request):
     return render(request, template_name='app/main_page.html')
 
-def login(request):
+
+def app_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('app_index')
     return render(request, template_name='app/login.html')
+
+
+def app_logout(request):
+    logout(request)
+    return redirect('app_login')
