4f29b346b8c9b7e13d9da57b34f8a527f110ee6e
Vasco Marmelo <vascomartinsmarmelo@gmail.com>
2024-04-24 10:14:04 +0100

    update search bar

--- _300Buy/app/static/polls/base_page.js
+++ _300Buy/app/static/polls/base_page.js
@@ -20,6 +20,24 @@ function RedirectLogo({ href }){
     )
 }
 
+function SearchFunction({ href }){
+
+    return (
+        <div className="container row" style={{textAlign:"center"}} >
+            <SearchBar href={href}/>
+            <div className='col-sm-1'/>
+            <SearchButton href={href}/>
+        </div>
+    )
+}
+
+function SearchBar({ href }){
+
+    return (
+        <input id="searchBoxMain" type="text" className="col-sm-8" style={{height: "2.5em"}}></input>
+    )
+}
+
 function SearchButton({ href }){
 
     const btnClicked = () => {
@@ -29,11 +47,11 @@ function SearchButton({ href }){
     };
 
     return (
-        <button onChange={handleChange} value={searchText} onClick={btnClicked} type="btn" className="btn container" style={{width: "3em", height: "2.5em", backgroundColor: "rgba(2, 171, 73)"}}>
+        <button onClick={btnClicked} type="btn" className="btn col-sm-2" style={{width: "3em", height: "2.5em", backgroundColor: "rgba(2, 171, 73)"}}>
             <svg style={{display: "block", margin: "auto"}} xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMin" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
             </svg>
-        </button>
+        </button>   
     )
 }
 
@@ -43,5 +61,5 @@ ReactDOM.render(<RedirectButton href={'/login'} />, domContainerLoginBtn);
 const domContainerLogoImg = document.querySelector('#logo');
 ReactDOM.render(<RedirectLogo href={'/'} />, domContainerLogoImg);
 
-const domContainerSearchBtn = document.querySelector('#searchButton');
-ReactDOM.render(<SearchButton href={'/'} />, domContainerSearchBtn);
+const domContainerSearchFunction = document.querySelector('#searchBar');
+ReactDOM.render(<SearchFunction href={'/'} />, domContainerSearchFunction);
