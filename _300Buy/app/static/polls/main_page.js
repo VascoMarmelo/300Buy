5c26e3be88222520c5164406fed9e5f58068b573
Vasco Marmelo <vascomartinsmarmelo@gmail.com>
2024-04-24 15:05:31 +0100

    Search bar working

--- _300Buy/app/static/polls/main_page.js
+++ _300Buy/app/static/polls/main_page.js
@@ -2,25 +2,33 @@
 function Products() {
     const [productsList, setProductsList] = React.useState([]);
     const searchParams = new URLSearchParams(window.location.search);
-    const productFilter = searchParams.get('filter');
+    let productFilter = searchParams.get('filter');
+
+    if (productFilter == null){
+      productFilter = "";
+    }
+    else{
+      productFilter = "/" + productFilter; 
+    }
 
     //Adicionar o filtro correto
-
     React.useEffect(() => 
-        fetch("api/products/")
+        fetch("api/products" + productFilter)
         .then(response => response.json())
         .then(json => {
           console.log(json);
-
           setProductsList(json);
       }), []);
 
     return (
-      <div className="container">
-        <div className="row">
-          {productsList.map(product => {
-            return <Product key={product.id} product={product} />
-          })}
+      <div className="container ">
+        <div className="row d-flex justify-content-center">
+          <ul>
+            {       
+            productsList.map(product => {
+              return <Product key={product.id} product={product} />
+            })}
+          </ul>
         </div>
       </div>
     );
@@ -28,14 +36,20 @@ function Products() {
 
 function Product({product}){
 
+  const btnClicked = () => {
+    console.log("OLA");
+  }
+
 
   return (
-    <div className="col-sm" style={{'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', textAlign: 'center'}}>
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        <img src="https://i.imgur.com/FKLhQ3T.png" style={{height: '10em', width: '10em'}}></img>
-        <p style={{'justifyContent': 'center'}} className="row">{product.title}</p>
-      </div>   
-    </div>
+    <li>
+      <div className="col-sm" style={{'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', textAlign: 'center'}}>
+        <div style={{display: 'flex', flexDirection: 'column'}} onClick={{btnClicked}}>
+          <img src="https://i.imgur.com/FKLhQ3T.png" style={{height: '10em', width: '10em'}}></img>
+          <p style={{'justifyContent': 'center'}} className="row">{product.title}</p>
+        </div>
+      </div>
+    </li> 
   )
 }
 
